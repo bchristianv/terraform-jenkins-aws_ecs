2f3a918ad039f1897e00630072e98cefa21193bd
BChristianV <bchristianv@icloud.com>
2020-11-16 14:38:38 -0800

    Add basic provider configuration.

---
+++ provider.tf
@@ -0,0 +1,15 @@
+# Main terraform provider configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>2.70"
+    }
+  }
+
+}
+
+provider "aws" {
+  region  = var.aws_region
+}
